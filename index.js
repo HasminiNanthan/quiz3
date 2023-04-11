3abfd202a4cd9c72e6f7c0c47f72d112e1224ba0
Hasmini <hasmininanthan023@gmail.com>
2023-04-11 14:41:05 +0800

    login

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+let dbUsers = [
+  {
+      username: "hasmini",
+      password: "H@s230798",
+      name: "Hasmini",
+      email: "hasmininanthan023@gmail.com"
+  
+  },
+  {
+      username: "john",
+      password: "password",
+      name: "John",
+      email: "john@utem.edu.my"
+  
+  },
+  {
+      username: "ali",
+      password: "230798",
+      name: "Ali",
+      email: "ali@utem.edu.my"
+  
+  }
+  ]
+
+function login(reqUsername, reqPassword) {
+  let matchUser = dbUsers.find(user => user.username == reqUsername)
+
+  //console.log(matchUser)
+
+if(!matchUser) return "User not found!"
+
+if(matchUser.password == reqPassword){
+  return matchUser
+}else{
+  return "Invalid password"
+}
+}
+
+function register (reqUsername,reqPassword,reqName,reqEmail){
+  dbUsers.push({
+      username: reqUsername,
+      password: reqPassword,
+      name: reqName,
+      email: reqEmail
+
+  })
+
+
+}
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,7 +57,9 @@ app.use(express.json())
 app.post('/login', (req, res) => {
     console.log(req.body)
 
-    res.send('Login')
+    let result = login(req.body.username,req.body.password)
+
+    res.send(result)
 })
 
 app.get('/', (req, res) => {
@@ -19,7 +71,14 @@ app.get('/bye', (req, res) => {
   })
 
 app.post('/register', (req, res) => {
-    res.send('ACCOUNT CREATED!')
+  let result = register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )  
+  
+  res.send(result)
 })
 
 
